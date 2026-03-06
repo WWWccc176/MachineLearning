ae4e9596edad08d628acb80d863613f156d3d407
lccc372 <lccc372@gmail.com>
2026-03-06 11:35:29 +0800

    3.6man

---
+++ MTH129/Topic1/calcu.py
@@ -0,0 +1,13 @@
+a = (((6 + 5) - 4) * 3 / 2) ** 2 % 4
+print(a)
+
+b = "Hello World!"
+c = ""
+for i in b:
+    c = c + i
+    print(c)
+
+y = [0] * 3
+for i in range(3):
+    y[i] = i * 2
+    print(y)
---
+++ MTH129/Topic1/lab1.py
@@ -0,0 +1,10 @@
+import torch
+import numpy as np
+
+print(torch.__version__)
+
+print(np.round(4.7))
+print(np.round(4.5))
+print(np.round(3.5))
+print(np.round(-4.5))
+print(np.round(0.5))

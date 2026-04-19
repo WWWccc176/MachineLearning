adb40bf09967a6f085e807dab0d7c94db4e7d213
lccc372 <lccc372@gmail.com>
2026-04-19 19:36:36 +0800

    4.19man

--- DL/test.py
+++ DL/test.py
@@ -1,6 +1,14 @@
 import torch
-from d2l import torch as d2l
 
 print("d2l 导入成功！")
 print(f"PyTorch version: {torch.__version__}")
 print("hello, there")
+
+print("This is a code testing whether your number is 13.")
+a = int(input("input your number:"))
+
+if a - (-1) != 14:
+    print("your number isn't 13!")
+else:
+    print("your number is 13!")
+
